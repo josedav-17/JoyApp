b74330bc000f11d6d3b4baea9b7c95f6ff0a720c
Jose David <jdcardona58@gmail.com>
2025-03-24 18:48:44 -0500

    pruebas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React, { useEffect } from "react";
+import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
-import ReactGA from "react-ga4";
 import './index.css';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 function App() {
-  useEffect(() => {
-    ReactGA.initialize("G-HC4N5337BV");
-    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
-  }, []);
-
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
