528d044f450fae51143549e438952f0ded4afd14
Jose David <jdcardona58@gmail.com>
2025-03-24 18:14:13 -0500

    AJUSTES

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import ReactGA from "react-ga4";
 import './index.css';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-HC4N5337BV"); // Reemplaza con tu ID real
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: false,
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
