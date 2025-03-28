83df94c5b88897166d2f0f221d3e78b2088539d3
Jose David <jdcardona58@gmail.com>
2025-03-27 20:22:53 -0500

    ajustes de correo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#F2A81D] text-white p-4 text-center">
       <p>&copy; {new Date().getFullYear()} Joy Riders SAS. Todos los derechos reservados.</p>
+      <p>Contacto: <a href="mailto:joyriders.col@gmail.com" className="underline">contacto@joyriders.com</a></p>
     </footer>
   );
 };
