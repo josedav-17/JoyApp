e91a45cda1390883980e84657095bd3086d98b5e
Jose David <jdcardona58@gmail.com>
2025-03-27 20:23:53 -0500

    ajustes de correo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-[#F2A81D] text-white p-4 text-center">
-      <p>&copy; {new Date().getFullYear()} Joy Riders SAS. Todos los derechos reservados.</p>
+      <p>Joy Riders SAS &copy; {new Date().getFullYear()}</p>
       <p>Contacto: <a href="mailto:joyriders.col@gmail.com" className="underline">contacto@joyriders.com</a></p>
     </footer>
   );
